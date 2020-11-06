f6b3c5d6dbc2f1080256883c44637555fb44b3de
danegorov <egdanik171@yandex.ru>
2020-11-07 00:59:45 +0300

    practice6

--- src/com/practice10/Main.java
+++ src/com/practice10/Main.java
@@ -1,12 +1,14 @@
 package com.practice10;
 
 import java.awt.event.MouseAdapter;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //four();
         //five();
+        seven(20);
 
 
     }
@@ -46,5 +48,19 @@ public class Main {
     public static int sumdigits(int a){
         if (a / 10 == 0) return a;
         return a % 10 + sumdigits(a / 10);
+    }
+    public static void seven(int a){
+        boolean[] arr = new boolean[a + 1];
+        System.out.println(arr[3]);
+        for (int i = 2; i <= a; i++){
+            if (!arr[i]) {
+                for (int j = i * 2; j <= a; j = i * j) {
+                    arr[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < a + 1; i++){
+            if(!arr[i]) System.out.println(Integer.toString(i) + ' ');
+        }
     }
 }
